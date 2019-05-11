ce576c29e0273ca969aae3523cff274be1bb8e48
adrianhindes <adrian.hindes@anu.edu.au>
2019-05-11 15:12:44 +1000

    Add files via upload

--- lotka-volterra.py
+++ lotka-volterra.py
@@ -12,7 +12,7 @@ Following SciPy Tutorial:
 import numpy as np
 import pylab as p
 from scipy import integrate
-
+#----------------------------------------
 # Lotka Volterra parameters
 a = 1. # Prey growth rate
 b = 0.1 #Background prey mortality rate
@@ -20,7 +20,10 @@ c = 1.5 #Bockground predator mortality rate
 d = 0.75 #Predaction conversion efficiency
 
 # X[0] = prey, x[1] = predator
+#----------------------------------------
 
+# Lotka-Volterra equations and Jacobian
+#----------------------------------------
 def dX_dt(A, t=0):
     """ Return the growth rate of prey and predator population """
     x = A[0]
@@ -46,6 +49,7 @@ def jacX(A, t=0):
 # Calculate linearizations
 A_f0 = jacX(X_f0)
 A_f1 = jacX(X_f1)
+#----------------------------------------
 
 print("Extinction Jacobian: \n" + str(A_f0))
 print("Periodic Jacobian: \n" + str(A_f1))
@@ -82,5 +86,49 @@ p.title('Lotka-Volterra model periodic steady state')
 # Importantly, you cannot get a time series out of
 # generalised models because there is no functional forms specified
 # for an ode solver to do its thing
+
+#----------------------------------------
+# Plotting phase plane and trajectories
+#----------------------------------------
+
+values = np.linspace(0.3, 0.9, 5) # X0 in values between stable points
+vcolours = p.cm.autumn_r(np.linspace(0.3, 1, len(values))) #colours for trajectories
+
+f2 = p.figure()
+#Plot trajectories
+for v, col in zip(values, vcolours):
+    X0 = v * X_f1 #start point
+    X = integrate.odeint(dX_dt, X0, t) #integrate
+    p.plot(X[:,0], X[:,1], lw=3.5*v, color=col, label='X0=(%.f, %.f)' % (X0[0], X0[1]))
+
+#Define grid and compute direction at each ponit
+    # Get axes limits
+ymax = p.ylim(ymin=0)[1] 
+xmax = p.xlim(xmin=0)[1]
+nb_points = 20
+
+x = np.linspace(0, xmax, nb_points)
+y = np.linspace(0, ymax, nb_points)
+
+X1, Y1 = np.meshgrid(x, y) # create grid
+DX1, DY1 = dX_dt([X1,Y1]) # compute infinitesimal growth rate at each point in grid
+M = (np.hypot(DX1,DY1) ) #slope for each point
+M[M==0] = 1. # Avoid zeros
+DX1 /= M #Normalize arrow components
+DY1 /= M
+
+
+#----------------------------------------
+# Draw direction fields, use plt's quiver function
+# Colours correspond to speed, can also remove normalization
+
+p.title('Phase space of Lotka-Volterra equations')
+Q = p.quiver(X1, Y1, DX1, DY1, M, pivot='mid', cmap=p.cm.jet)
+p.xlabel('Population of Prey')
+p.ylabel('Population of Predators')
+p.legend()
+p.grid()
+p.xlim(0, xmax)
+p.ylim(0, ymax)
 
 
